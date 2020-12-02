696bc1f4809c14578cdb98a621e7f5e97afc89f6
Xeroneon <andrewvasquez0113@gmail.com>
2020-12-02 12:10:36 -0700

    style: :art: added base styles for nav

--- components/Nav.js
+++ components/Nav.js
@@ -1,8 +1,11 @@
 export default function nav(props) {
     return (
         <>
-            <nav>
-                SeelieCourtDesigns
+            <nav className='flex flex-row shadow justify-between p-7'>
+                <h1 className='text-xl text-gray-700 font-bold'>SeelieCourtDesigns</h1>
+                <a>
+                    <p className='text-xl text-gray-700 font-bold'>Commissions</p>
+                </a>
             </nav>
         </>
     )

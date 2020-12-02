f90d1089c1705a44c24cbd1dc84bdf26e4e61838
Xeroneon <andrewvasquez0113@gmail.com>
2020-12-02 12:34:56 -0700

    refactor: Moved Nav so it shows on all pages

--- components/Nav.js
+++ components/Nav.js
@@ -1,7 +1,7 @@
 export default function nav(props) {
     return (
         <>
-            <nav className='flex flex-row shadow justify-between p-7'>
+            <nav className='flex flex-row shadow justify-between p-7 bg-pink-50'>
                 <h1 className='text-xl text-gray-700 font-bold'>SeelieCourtDesigns</h1>
                 <a>
                     <p className='text-xl text-gray-700 font-bold'>Commissions</p>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,11 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import Nav from 'components/Nav';
 import Hero from 'components/Hero';
 
 export default function Home() {
   return (
     <main>
-      <Nav />
       <Hero />
     </main>
 

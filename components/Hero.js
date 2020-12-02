206ce9c33e5912a80464ae031dc49d4689d8900e
Xeroneon <andrewvasquez0113@gmail.com>
2020-12-02 12:29:06 -0700

    style: :art: added basis for hero image

---
+++ components/Hero.js
@@ -0,0 +1,9 @@
+export default function Hero() {
+    return (
+        <>
+            <div className='w-full' style={{height: '500px'}}>
+                <img src='assets/images/seelie-hero.jfif' className='w-full h-full object-cover overflow-hidden' />
+            </div>
+        </>
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,14 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
 import Nav from 'components/Nav';
+import Hero from 'components/Hero';
 
 export default function Home() {
   return (
-    <Nav />
+    <main>
+      <Nav />
+      <Hero />
+    </main>
+
   )
 }

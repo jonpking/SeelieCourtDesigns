194d9b39630b8b047c01bd410279cff28128cf22
Jonathan King <jonpking@gmail.com>
2020-12-07 23:13:22 -0700

    removed folder that is no longer needed

--- pages/api/contentful/getImages.js
+++
@@ -1,7 +0,0 @@
-import client from 'services/contentful';
-
-export default (req, res) => {
-    client
-        .getEntries("galleryItem")
-        .then(entries => res.status(200).json(entries.items));
-}

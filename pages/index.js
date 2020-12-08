efe306b24d2d16545c80651e987ee85847ffc2f8
Jonathan King <jonpking@gmail.com>
2020-12-07 23:13:56 -0700

    cleaned up code, added images to props

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,18 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
-
 import Hero from 'components/Hero';
 import client from 'services/contentful';
 
-export default function Home() {
+export default function Home(props) {
+  console.log(props);
   return (
     <main>
       <Hero />
     </main>
-
   )
 }
 
 export async function getServerSideProps(context) {
   const images = await client.getEntries("galleryItem");
-  console.log(images);
   return {
-    props: {}, // will be passed to the page component as props
+    props: {images}, // will be passed to the page component as props
   }
 }
